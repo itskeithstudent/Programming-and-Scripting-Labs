f42cce8e557720f0f230ec84b4d01874dc425500
itskeith <itskeithryan@gmail.com>
2020-02-06 21:40:26 +0000

    add logic to capture event someone enters a number less than 0 or greater than 100, also add comments

--- Week 4/Lab04.01-grade.py
+++ Week 4/Lab04.01-grade.py
@@ -1,13 +1,17 @@
 #program to  read students mark and print whether pass or fail
 
 student_grade = int(input("Tell me what percentage you got and I'll tell you the grade..."))
-if student_grade < 40:
+
+#check first that we got a possible grade between 0 and 100
+if student_grade < 0 or student_grade > 100:
+    print("Enter a possible number dumbo!")
+elif student_grade < 40: #less than 40
     print("FAIL")
-elif student_grade < 49:
+elif student_grade < 49: #less than 49
     print("PASS")
-elif student_grade < 59:
+elif student_grade < 59: #less than 59
     print("Merit 2")
-elif student_grade < 69:
+elif student_grade < 69: #less than 69
     print("Merit 1")
-else:
+else: #70 or greater
     print("Distinction")
