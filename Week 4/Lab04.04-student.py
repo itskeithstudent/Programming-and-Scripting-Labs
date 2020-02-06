48099f1ad074f85e5361b74b75f0a2e17fd1a725
itskeith <itskeithryan@gmail.com>
2020-02-06 22:22:08 +0000

    Lab4 exercise 4 added strip to end of user submitted strings

--- Week 4/Lab04.04-student.py
+++ Week 4/Lab04.04-student.py
@@ -2,7 +2,8 @@
 #will assume it's a name even if numbers are entered, so take user input as str
 
 #take names from user
-studentf_name = str(input("Enter a student first name please (BLANK to quit): ")) # we will only exit if first name is blank could have individuals like Cher with no last name
+#to prevent case where space is used in place of blank, we strip the string so it is blank anyway
+studentf_name = str(input("Enter a student first name please (BLANK to quit): ")).strip() # we will only exit if first name is blank could have individuals like Cher with no last name
 entered_students = [] #list of entered numbers
 if studentf_name == '': #if user has entered a '' we proceed no further
     print("You entered no name")
@@ -10,11 +11,11 @@ else: #else user must not have entered a blank
     while studentf_name !='': #while user hasn't entered a blank
         #this list gets reassigned to empty at start of each loop
         student_name_list = []
-        studentl_name = str(input("Enter a student last name please: "))
+        studentl_name = str(input("Enter a student last name please: ")).strip()
         student_name_list.append(studentf_name)
-        student_name_list.append(studentl_name) 
+        student_name_list.append(studentl_name)
         entered_students.append(student_name_list)#we don't need to add 0 to the end of the list when a user enter 0
-        studentf_name = str(input("Enter a student name please (BLANK to quit): "))
+        studentf_name = str(input("Enter a student first name please (BLANK to quit): ")).strip()
     print("Here are the names of your students back again")
     #for loop over entered_students list, print first 2 index's of student
     for student in entered_students:
