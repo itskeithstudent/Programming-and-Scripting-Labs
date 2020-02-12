5dd3be9a4de3d7fbe9a971f138ad5a743f2aa38c
itskeith <itskeithryan@gmail.com>
2020-02-12 20:47:25 +0000

    Add first lab week 5

---
+++ Topic05-datastructures/Lab05.01.py
@@ -0,0 +1,6 @@
+#Add all months to tuple list
+months_tuple = ("January","February","March","April","May","June","July","August","September","October","November","December")
+#assign months 4,5 and 6 to summer_months_tuple
+summer_months_tuple = months_tuple[4:7]
+#trying out list comprehension to do as oneliner
+[print(x) for x in summer_months_tuple]
